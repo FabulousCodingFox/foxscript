c38d8638a28f24a2b7e56dae8ab062211ba9e479
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2021-09-10 23:18:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+##############################################################################################################################################################
 class Function:
     def __init__(self, code="", mccode="", path="", name="null",namespace=""):
         self.code = code
@@ -5,6 +6,7 @@ class Function:
         self.path=path
         self.name=name
         self.namespace=namespace
+##############################################################################################################################################################
 class Namespace:
     def __init__(self, name="main", rawcode="", path="", functions=[]):
         self.name = name
@@ -13,6 +15,56 @@ class Namespace:
         self.rawcode=rawcode
     def add_function(self,function):
         self.functions.append(function)
+##############################################################################################################################################################
+class Player:
+    def __init__(self,name=None,type="@a",advancements=None,distance=None,dx=None,dy=None,dz=None,gamemode=None,level=None,limit=None,nbt=None,predicate=None,scores=None,sort=None,tag=None,team=None,x=None,x_rotation=None,y=None,y_rotation=None,z=None):
+        string=type+"["
+        if not advancements==None:string=string+"advancements="+str(advancements)+","
+        if not distance==None:string=string+"distance="+str(distance)+","
+        if not dx==None:string=string+"dx="+str(dx)+","
+        if not dy==None:string=string+"dy="+str(dy)+","
+        if not dz==None:string=string+"dz="+str(dz)+","
+        if not gamemode==None:string=string+"gamemode="+str(gamemode)+","
+        if not level==None:string=string+"level="+str(level)+","
+        if not limit==None:string=string+"limit="+str(limit)+","
+        if not name==None:string=string+"name="+str(name)+","
+        if not nbt==None:string=string+"nbt="+str(nbt)+","
+        if not predicate==None:string=string+"="+str(predicate)+","
+        if not scores==None:string=string+"="+str(scores)+","
+        if not sort==None:string=string+"="+str(sort)+","
+        if not tag==None:string=string+"tag="+str(tag)+","
+        if not team==None:string=string+"team="+str(team)+","
+        if not x==None:string=string+"x="+str(x)+","
+        if not x_rotation==None:string=string+"x_rotation="+str(x_rotation)+","
+        if not y==None:string=string+"y="+str(y)+","
+        if not y_rotation==None:string=string+"y_rotation="+str(y_rotation)+","
+        if not z==None:string=string+"z="+str(z)+","
+        if string[-1]!="[":string=string[:-1]+"]"
+        else:string=string[:-1]
+        self.selector=string
+    def __str__(self):
+        return self.selector
+##############################################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def error(line,linenumber,text):global err;print("\nError in line "+str(linenumber)+":\nLine: "+str(line)+":\nText: "+str(text));err=True
 
@@ -119,72 +171,110 @@ def generate_data_structures(code):
 
 
 
+def compile_function(func):
+    this=""
+    for linenumber in range(len(func.code.rstrip().split("\n"))):
+        line = func.code.split("\n")[linenumber]
+
+        if not line.startswith("    "):
+            ##################################################################################################################################################
+            if line.strip().startswith("scoreboard("):
+                try:
+                    def scoreboard(name="main",type="dummy",operation="add"):return "scoreboard objectives "+operation+" "+name+" "+type
+                    this = this + eval(line.strip()) + "\n"
+                except:error(line.strip(),linenumber,"Incorrect usage of scoreboard()")
+            ##################################################################################################################################################
+            if line.strip().startswith("say("):
+                try:
+                    def say(text):return "say "+str(text)
+                    this = this + eval(line.strip()) + "\n"
+                except:error(line.strip(),linenumber,"Incorrect usage of say()")
+            ##################################################################################################################################################IF
+            if line.strip().startswith("if "):
+                def get_current_if():
+                    lambda w:w.translate(dict(zip(s,s[::-1])))
+                    files=os.listdir(func.namespace.path);highest="if_";s=False
+                    for i in files:
+                        if "if_" in i:s=True
+                    if s==False:return "if_"
+                    for i in files:
+                        if "if_" in i and i>highest:highest=i
+                    if highest!="if_.mcfunction":
+                        letters=i[3]
+                        if letters=="":lettersn=0
+                        if letters=="a":lettersn=1
+                        if letters=="b":lettersn=2
+                        if letters=="c":lettersn=3
+                        if letters=="d":lettersn=4
+                        if letters=="e":lettersn=5
+                        if letters=="f":lettersn=6
+                        if letters=="g":lettersn=7
+                        if letters=="h":lettersn=8
+                        if letters=="j":lettersn=9
+                        if letters=="k":lettersn=10
+                        return "if_"+["a","b","c","d","e","f","g","h","i","j","k"][lettersn+1]
+                    else:
+                        return "if_a"
+                            
+                iffilepath = func.namespace.path+get_current_if()+".mcfunction"
+                iffunctionpath = func.namespace.name+":"+get_current_if()
+                if "==" in line.strip():
+                    statements=line.strip().split("==")
+                    statements=[statements[0].replace("if ","").strip(),statements[1][:-1].strip()]
+                    try:int(statements[0]);statements.append(True)
+                    except:statements.append(False)
+                    try:int(statements[1]);statements.append(True)
+                    except:statements.append(False)
+                    if statements[2]==True and statements[3] ==True: error(line.strip(),linenumber,"No two numbers in an if statement!")
+                    if statements[2]==True and statements[3]==False: statements=[statements[1],statements[0],statements[3],statements[2]]
+                    
+                    if "Player(" in statements[0]:
+                        elements=statements[0].split(".");elements=[i.strip() for i in elements]
+                        if not elements[1] in ["nbt"]:
+                            this = this + "execute if score "+str(eval(elements[0]))+" "+str(elements[1])+" "
+                            if statements[3] == True:
+                               this = this + "matches "+statements[1]+" run function "+iffunctionpath+"\n"
+
+
+                looplinenumber=linenumber;loopline="    ";this=""
+                while loopline.startswith("    "):
+                    looplinenumber+=1;loopline = func.code.split("\n")[looplinenumber]
+                    if loopline.startswith("    "):this = this + loopline[4:] + "\n"
+
+                thisf=Function(this,"",iffilepath,get_current_if(),func.namespace)
+                func.namespace.add_function(thisf)
+                open(iffilepath,"x")
+                compile_function(thisf)
+            ##################################################################################################################################################IF
+
+                    
+
+                    
+        
+    return this
 
 
 
 
-
-
-
-
-def seperate_func(code):
-    ###Seperating
-    namespaces=[]
-    for line in range(len(code.split("\n"))):
-        this=""
-        if code.split("\n")[line].startswith("class "):
-            name=code.split("\n")[line].split(" ")[1][:-1]
-            thisotherline=" "
-            j=line
-            while thisotherline.startswith(" "):
-                j+=1
-                thisotherline=code.split("\n")[j]
-                if thisotherline.startswith(" "):
-                     this=this+thisotherline[4:]+"\n"
- 
-            os.mkdir("output/" + pr["name"] + "/data/" + name)
-            namespaces.append(Namespace(name=name,rawcode=this,path="output/" + pr["name"] + "/data/" + name + "/"))
-    ###Seperating Functions
-    for namespace in namespaces:
-        nscode = namespace.rawcode
-        for line in range(len(nscode.split("\n"))):
-            code=""
-            if nscode.split("\n")[line].startswith("def "):
-                funcname = nscode.split("\n")[line].split(" ")[1][:-1]
-                currentcodeline = "    "
-                linenumber=line
-                while currentcodeline.startswith("    "):
-                    linenumber+=1
-                    currentcodeline = nscode.split("\n")[linenumber]
-                    code = code + currentcodeline[4:] + "\n"
-                thisfunc=Function(code,"",namespace.path+funcname+".mcfunction",funcname,namespace)
-                if thisfunc.name =="__tick__":__tick__.append(thisfunc)
-                elif thisfunc.name =="__load__":__load__.append(thisfunc)
-                namespace.add_function(thisfunc)
-                open(namespace.path+funcname+".mcfunction","x")
-                
-    return pr,namespaces,__tick__,__load__
-
-
-
+      
 starttime=time.time()
 
-code="""
-__project__["mc-version"]  = "1.16.5"
-__project__["name"]        = "Fox"
-__project__["description"] = "FoxSciptTest"
-__project__["author"]      = "FabulousFox"
-
-class Main: 
-    def __tick__():
-        say("Main.Tick")
-    def __load__():
-        say("Main.Load")
-"""
+f=open("source.fs","r");code=str(f.read());f.close()
 
 print("Checking for Syntax errors...")
 c=check_for_syntax_error(code)
 print("Generating Data Structures(Pre-Compile)...")
-pr,namespaces,_tick_,_load_generate_data_structures(c)
+pr,namespaces,_tick_,_load_=generate_data_structures(c)
+
+
+def function_to_mcpath(func):return func.namespace.name+":"+func.name
+for namespace in namespaces:
+    for function in namespace.functions:
+        this=compile_function(function)
+        function.mccode=this
+        f=open(function.path,"w");f.write(this);f.close()
+f=open("output/" + pr["name"] + "/data/minecraft/tags/functions/tick.json","w");f.write("{\"values\":"+str(list([str(i.namespace.name+":"+i.name) for i in _tick_]))+"}");f.close()
+f=open("output/" + pr["name"] + "/data/minecraft/tags/functions/load.json","w");f.write("{\"values\":"+str(list([str(i.namespace.name+":"+i.name) for i in _load_]))+"}");f.close()
+
 
 print("Finished in",time.time()-starttime,"Seconds")

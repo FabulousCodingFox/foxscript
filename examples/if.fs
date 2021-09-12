8a224d1140ebad17762c6f2d6c8d47a2bba798e4
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2021-09-12 12:42:42 +0200

    Create if.fs

---
+++ examples/if.fs
@@ -0,0 +1,13 @@
+__project__["mc-version"]  = "1.16.5"
+__project__["name"]        = "if"
+__project__["description"] = ""
+__project__["author"]      = "FabulousFox"
+
+class if: 
+    def __load__():
+        scoreboard("main")
+        Player(selector="@p").main = 0
+        if Player(selector="@p").main == 3:
+            say("Score = 3")
+            if Player(selector="@p").main == Player(selector="@p").main:
+                say("sus")

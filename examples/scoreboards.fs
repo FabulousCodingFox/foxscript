29954e89576a6ade3f27d05984e54a979423ae15
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2021-09-12 12:38:13 +0200

    Update scoreboards.fs

--- examples/scoreboards.fs
+++ examples/scoreboards.fs
@@ -1,7 +1,12 @@
-class Main:
-    def __load__():
-        scoreboard("points")
-        Player(selector="@p",name="Steve").points = 100
+__project__["mc-version"]  = "1.16.5"
+__project__["name"]        = "scoreboards"
+__project__["description"] = "Scoreboard example"
+__project__["author"]      = "FabulousFox"
+
+class scoreboards: 
     def __tick__():
-        Player(selector="@p",name="Steve").points += 10
-        Player(selector="@p",name="Steve").points *= Player(selector="@p",name="Alex").points
+        Player().main+=1#Adding 1 to main for everyone
+    def __load__():
+        scoreboard("main")#Creating the scoreboard "main"
+        Player().main = 5#Setting the Scoreboard for everyone to 5
+        setdisplay("main")#Displaying the scoreboard
